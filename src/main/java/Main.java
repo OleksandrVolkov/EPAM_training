caf152c75b83cf2eec13a45a7846dc5ec9a93392
oleksandr_volkov <oleksandr.volkov.oov@gmail.com>
2018-10-21 03:23:37 +0300

    Main class is deleted

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import task_2.task_2_2.controller.MainController;
-import task_3.task_3_2.controller.Controller;
-//import task_2.task_2_1.controller.Controller;
-
-public class Main {
-    public static void main(String [] argv){
-
-    }
-}
