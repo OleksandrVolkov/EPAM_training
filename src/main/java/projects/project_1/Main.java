05d7c7b5ebe3cb90bd7d507a84930877f165f613
oleksandr_volkov <oleksandr.volkov.oov@gmail.com>
2018-10-31 00:22:45 +0200

    Main class is deleted

--- src/main/java/projects/project_1/Main.java
+++
@@ -1,9 +0,0 @@
-package projects.project_1;
-
-import projects.project_1.controller.Controller;
-
-public class Main {
-    public static void main(String []args){
-        new Controller().run();
-    }
-}
--- src/main/java/projects/project_1/view/UserInterface.java
+++
@@ -1,5 +0,0 @@
-package projects.project_1.view;
-
-public interface UserInterface {
-    String INITIAL_MES = "Starting a program";
-}
